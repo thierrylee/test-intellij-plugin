48e1768027447d2396ef99ed4bab1de5b792d86f
Thierry LEE <thierry.lee@octo.com>
2025-04-15 16:53:25 +0200

    Add class documentation to file

--- src/main/kotlin/com/github/thierrylee/testintellijplugin/actions/AddTotoAction.kt
+++ src/main/kotlin/com/github/thierrylee/testintellijplugin/actions/AddTotoAction.kt
@@ -3,13 +3,21 @@ package com.github.thierrylee.testintellijplugin.actions
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.openapi.command.WriteCommandAction
 
 class AddTotoAction: AnAction() {
 
     override fun actionPerformed(action: AnActionEvent) {
         val fileContent = action.getRequiredData(LangDataKeys.EDITOR).document.text
 
-        println("Methods :\n${findMethods(fileContent).joinToString("\n")}")
+        val classDoc = "/** Methods :\n * ${findMethods(fileContent).joinToString("\n * ")}\n */\n"
+        val classLineNumber = getClassLineNumber(fileContent)
+
+        WriteCommandAction.runWriteCommandAction(action.project) {
+            val document = action.getRequiredData(LangDataKeys.EDITOR).document
+            val charOffset = document.getLineStartOffset(classLineNumber)
+            document.insertString(charOffset, classDoc)
+        }
     }
 
     private fun findMethods(text: String): List<String> {
@@ -18,7 +26,11 @@ class AddTotoAction: AnAction() {
             .map { it.trim() }
             .map { it.substringAfter("fun").substringBefore("(").trim() }
             .filter { it.isNotEmpty() }
+    }
 
+    private fun getClassLineNumber(text: String): Int {
+        return text.split("\n")
+            .indexOfFirst { it.contains("class ") }
     }
 
 }
