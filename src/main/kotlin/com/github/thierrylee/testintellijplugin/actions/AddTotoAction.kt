0d71cc8477d2aa6daca6b7f24305953ff55bd06c
Thierry LEE <thierry.lee@octo.com>
2025-04-15 15:54:09 +0200

    Add findMethods

--- src/main/kotlin/com/github/thierrylee/testintellijplugin/actions/AddTotoAction.kt
+++ src/main/kotlin/com/github/thierrylee/testintellijplugin/actions/AddTotoAction.kt
@@ -2,11 +2,23 @@ package com.github.thierrylee.testintellijplugin.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.LangDataKeys
 
 class AddTotoAction: AnAction() {
 
     override fun actionPerformed(action: AnActionEvent) {
-        print("Toto !!")
+        val fileContent = action.getRequiredData(LangDataKeys.EDITOR).document.text
+
+        println("Methods :\n${findMethods(fileContent).joinToString("\n")}")
+    }
+
+    private fun findMethods(text: String): List<String> {
+        return text.split("\n")
+            .filter { it.contains("fun") }
+            .map { it.trim() }
+            .map { it.substringAfter("fun").substringBefore("(").trim() }
+            .filter { it.isNotEmpty() }
+
     }
 
 }
